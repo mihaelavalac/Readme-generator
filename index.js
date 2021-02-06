3c9b4a2945455604bab1c016028a3aad8e0d5012
Mihaela <mihaelavalac@yahoo.com>
2021-02-05 22:01:54 -0500

    Crated the function that bulds the README file

--- index.js
+++ index.js
@@ -9,58 +9,111 @@ const askUser = () =>
   inquirer.prompt([
     {
       type: "input",
-      name: "gitHubUserName",
+      name: "github",
       message: "What is your Github username?"
     },
     { type: "input",
-      name: "Email",
+      name: "email",
       message: "What is your Email address?"
     },
     { 
       type: "input",
-      name: "projectTitle",
+      name: "title",
       message: "What is the title of your project?"
     },
     {
       type: "input",
-      name: "Description",
+      name: "description",
       message: "How can you describe your project?"
     },
     {
       type: "input",
-      name: "Installation",
+      name: "installation",
       message: "How to install yur application?"
     }, 
     {
       type: "input",
-      name: "Usage",
+      name: "usage",
       message: "How to use your application?"
     },
     { 
       type: "input",
-      name: "License",
-      message: "//LOOK FOR A LIST OF LICENSES"
+      name: "license",
+      message: "What kind of License should your project have?",
+      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
     },
     {
       type: "input",
-      name: "Contributions",
+      name: "contributions",
       message: "How can the user contribute to your project?"
     }, 
     {
       type: "input",
-      name: "Test",
+      name: "test",
       message: "How can the user test your application?"
     },
     { 
       type: "input",
-      name: "gitHubUserName",
+      name: "questions",
       message: "How can the user contact you for questions?"
     }
     
   ]);
 
+
+
+
 // Generate the Readme file in the generate-readme folder.
 const generateReadme = (answers) => {
+  return ` # ${answers.title}
+  {}
+
+  ## Description
+
+  ${answers.description}
+
+  ## Table of Contents
+  
+  * [Installation](#installation)
+  
+  * [Usage](#usage)
+  
+  * [Contributing](#contributing)
+  
+  * [Test] (#test)
+  
+  * [Questions] (#questions)
+  
+  ## Installation 
+  
+  To install necessary dependencies, run the following command:
+  
+  \`\`\`
+  ${answers.installation}
+  \`\`\`
+  
+  ## Usage
+  ${answers.usage}
+  
+  ## Contributing
+  
+  ${answers.contributing}
+  
+  ##Tests
+  
+  To run tests, write the following command:
+  \`\`\`
+  ${answers.test}
+  \`\`\`
+  ## Questions
+
+  If you have any questions, don't hesitated to contact me at  ${
+    answers.email
+  }. Also you can find more of my projects at [${answers.github}](https://github.com/${
+    answers.github
+  }/)
+  
+  `
 
 }
 
